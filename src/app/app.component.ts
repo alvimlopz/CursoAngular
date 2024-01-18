36c39790b9aded1b33daa35c93ce844c42050cad
alvaro rodrigues <alvim.lopez@gmail.com>
2024-01-18 14:23:06 -0300

    compartilhando dados @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  userName = "joaquim"
+  userData = {
+    email: 'joaquim@gmail.com',
+    role: 'Admin'
+  }
+  idade = 68
   title = 'curso-angularYT';
 }
