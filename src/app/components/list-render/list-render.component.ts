6bd72a918fd8f000acbbe842edc28c2ad0962089
alvaro rodrigues <alvim.lopez@gmail.com>
2024-02-06 20:33:06 -0300

    criando interface

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,22 +1,30 @@
 import { Component } from '@angular/core';
 
+import { Animal } from '../../Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrl: './list-render.component.css'
 })
 export class ListRenderComponent {
-  animals = [
-    {name: 'turca', type: 'Dog'},
-    {name: 'Tom', type: 'Cat'},
-    {name: 'Frida', type: 'Dog'},
-    {name: 'Bob', type: 'Horse'},
+  animals: Animal[] =  [
+    {name: 'turca', type: 'Dog', age: 4},
+    {name: 'Tom', type: 'Cat', age: 14},
+    {name: 'Frida', type: 'Dog', age: 15},
+    {name: 'Bob', type: 'Horse', age: 20},
   ];
+
+  animalDetaisl = '';
 
   constructor(){
 
   }
 
   ngOnInit(): void {}
+
+  showAge(animal: Animal){
+this.animalDetaisl = `o pet ${animal.name} tem ${animal.age} anos`
+  }
 
 }
