c23fc29eb784fbfd859e913e60795e521ba770df
alvaro rodrigues <alvim.lopez@gmail.com>
2024-02-08 20:56:29 -0300

    Services do Angular

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { Animal } from '../../Animal';
+import { ListService } from '../../services/list.service';
 
 @Component({
   selector: 'app-list-render',
@@ -17,14 +18,19 @@ export class ListRenderComponent {
 
   animalDetaisl = '';
 
-  constructor(){
+  constructor(private listService: ListService){
 
   }
 
   ngOnInit(): void {}
 
   showAge(animal: Animal){
-this.animalDetaisl = `o pet ${animal.name} tem ${animal.age} anos`
+    this.animalDetaisl = `o pet ${animal.name} tem ${animal.age} anos`
+  }
+
+  removerAnimal(animal: Animal){
+    console.log("Removendo animal...")
+   this.animals = this.listService.remove(this.animals, animal);
   }
 
 }
